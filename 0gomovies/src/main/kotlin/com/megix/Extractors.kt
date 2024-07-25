30cb6011055b79ae8d79aa98183abfe5fe6bb9fd
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-07-25 16:57:45 +0530

    try

--- 0gomovies/src/main/kotlin/com/megix/Extractors.kt
+++ 0gomovies/src/main/kotlin/com/megix/Extractors.kt
@@ -2,41 +2,9 @@ package com.megix
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import org.json.JSONObject
+import com.lagradost.cloudstream3.extractors.Jeniusplay
 
-open class Asumanaksoy: ExtractorApi() {
-    override val name: String = "Asumanaksoy"
-    override val mainUrl: String = "https://cdn.asumanaksoy.com"
-    override val requiresReferer = false
-
-    override suspend fun getUrl(
-        url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        val doc = app.get(url).document
-        val scriptTag = doc.selectFirst("script:containsData(playerjsSubtitle)").toString()
-        val matchResult = Regex("""FirePlayer\|([^|]*)\|""").find(scriptTag)
-        val dataId = matchResult ?. groups ?. get(1) ?. value
-        val headers = mapOf("X-Requested-With" to "XMLHttpRequest")
-        val response = app.post(
-            "https://cdn.asumanaksoy.com/player/index.php?data=${dataId}&do=getVideo", 
-            headers = headers
-        ).text 
-        val jsonObject = JSONObject(response)
-
-        val securedLink = jsonObject.getString("securedLink")
-        
-        callback.invoke(
-            ExtractorLink(
-                this.name,
-                this.name,
-                securedLink,
-                referer = "",
-                quality = Qualities.Unknown.value,
-                isM3u8 = true,
-            )
-        )
-    }
+class Asumanaksoy: Jeniusplay() {
+    override var mainUrl = "https://cdn.asumanaksoy.com"
 }
+
