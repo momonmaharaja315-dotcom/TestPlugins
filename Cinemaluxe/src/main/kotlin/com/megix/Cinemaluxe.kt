f0a738ef4bcb88bd221f6d71bc251ac2eeca3199
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-07-27 17:23:51 +0530

    try

---
+++ Cinemaluxe/src/main/kotlin/com/megix/Cinemaluxe.kt
@@ -0,0 +1,16 @@
+package com.megix
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class Cinemaluxe: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        registerMainAPI(CinemaluxeProvider())
+        registerExtractorAPI(GDFlix())
+        registerExtractorAPI(Sharepoint())
+        registerExtractorAPI(HubCloud())
+    }
+}
