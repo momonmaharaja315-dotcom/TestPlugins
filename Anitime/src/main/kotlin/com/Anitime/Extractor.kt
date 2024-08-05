60a9a0ffaed01e995bb086d5c39df736a460dd64
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-08-05 12:57:42 +0530

    try

--- Anitime/src/main/kotlin/com/Anitime/Extractor.kt
+++ Anitime/src/main/kotlin/com/Anitime/Extractor.kt
@@ -36,6 +36,9 @@ class AbyssCdn : ExtractorApi() {
         val link = "https://$domain/$qPrefix$vidId"
         val headers = mapOf(
             "User-Agent" to "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.186 Mobile Safari/537.36",
+            "Accept-Encoding" to "gzip, deflate",
+            "Accept" to "*/*",
+            "Connection" to "keep-alive",
             "Referer" to "$url"
         )
 
