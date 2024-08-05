728c8623633b90bffb3d0dcaed6091f77c71dffc
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-08-05 11:37:39 +0530

    try

--- Anitime/src/main/kotlin/com/Anitime/Extractor.kt
+++ Anitime/src/main/kotlin/com/Anitime/Extractor.kt
@@ -23,35 +23,26 @@ class AbyssCdn : ExtractorApi() {
     ) {
         val qPrefix = "whw"
         val document = app.get(url).document
+        
         val responseText = document.toString()
         val base64Pattern = Regex("PLAYER\\(atob\\(\"(.*?)\"\\)")
         val base64Value = base64Pattern.find(responseText)?.groups?.get(1)?.value ?: ""
-        val text = base64Value.toString()
+        val decodedJson = base64Decode(base64Value)
+        val jsonObject = JSONObject(decodedJson)
+
+        val domain = jsonObject.getString("domain")
+        val vidId = jsonObject.getString("id")
+        val sources = jsonObject.getJSONArray("sources")
+        val link = "https://$domain/$qPrefix$vidId"
 
         callback.invoke (
             ExtractorLink (
                 this.name,
                 this.name,
-                text,
+                link,
                 referer = url,
                 Qualities.Unknown.value
             )
         )
-        // val decodedJson = base64Decode(base64Value)
-        // val jsonObject = JSONObject(decodedJson)
-
-        // val domain = jsonObject.getString("domain")
-        // val vidId = jsonObject.getString("id")
-        // val sources = jsonObject.getJSONArray("sources")
-        // val link = "https://$domain/$qPrefix$vidId"
-        // callback.invoke (
-        //     ExtractorLink (
-        //         this.name,
-        //         this.name,
-        //         link,
-        //         referer = url,
-        //         Qualities.Unknown.value
-        //     )
-        // )
     }
 }
