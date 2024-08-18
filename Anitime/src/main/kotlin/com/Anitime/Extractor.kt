4cdd2b4a592a08690eff198e2c2f6138b61bc760
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2024-08-18 20:01:48 +0600

    try

--- Anitime/src/main/kotlin/com/Anitime/Extractor.kt
+++ Anitime/src/main/kotlin/com/Anitime/Extractor.kt
@@ -16,6 +16,74 @@ class AbyssCdn : ExtractorApi() {
     override val mainUrl: String = "https://abysscdn.com"
     override val requiresReferer = true
 
+    private fun customDecode(input: String): String {
+        val charset = "RB0fpH8ZEyVLkv7c2i6MAJ5u3IKFDxlS1NTsnGaqmXYdUrtzjwObCgQP94hoeW+/="
+        var output = StringBuilder()
+        var i = 0
+        var enc1: Int
+        var enc2: Int
+        var enc3: Int
+        var enc4: Int
+
+        // Clean the input
+        var cleanedInput = input.replace("_", "")
+        cleanedInput = cleanedInput.replace(Regex("[^A-Za-z0-9+/=]"), "")
+
+        while (i < cleanedInput.length) {
+            enc1 = charset.indexOf(cleanedInput[i++])
+            enc2 = charset.indexOf(cleanedInput[i++])
+            enc3 = charset.indexOf(cleanedInput[i++])
+            enc4 = charset.indexOf(cleanedInput[i++])
+
+            enc1 = (enc1 shl 2) or (enc2 shr 4)
+            enc2 = ((enc2 and 15) shl 4) or (enc3 shr 2)
+            enc3 = ((enc3 and 3) shl 6) or enc4
+
+            output.append(enc1.toChar())
+            if (enc3 != 64) {
+                output.append(enc2.toChar())
+            }
+            if (enc4 != 64) {
+                output.append(enc3.toChar())
+            }
+        }
+
+        return decodeUtf8(output.toString())
+    }
+
+    private fun decodeUtf8(utf8String: String): String {
+        val decodedString = StringBuilder()
+        var i = 0
+
+        while (i < utf8String.length) {
+            val charCode = utf8String[i].toInt()
+
+            when {
+                charCode < 128 -> {
+                    decodedString.append(utf8String[i])
+                    i++
+                }
+                charCode > 191 && charCode < 224 -> {
+                    val charCode2 = utf8String[i + 1].toInt()
+                    decodedString.append(
+                        ((charCode and 31) shl 6 or (charCode2 and 63)).toChar()
+                    )
+                    i += 2
+                }
+                else -> {
+                    val charCode2 = utf8String[i + 1].toInt()
+                    val charCode3 = utf8String[i + 2].toInt()
+                    decodedString.append(
+                        ((charCode and 15) shl 12 or ((charCode2 and 63) shl 6) or (charCode3 and 63)).toChar()
+                    )
+                    i += 3
+                }
+            }
+        }
+
+        return decodedString.toString()
+    }
+
     override suspend fun getUrl(
         url: String,
         referer: String?,
@@ -26,31 +94,42 @@ class AbyssCdn : ExtractorApi() {
         val responseText = document.toString()
         val base64Pattern = Regex("parse\\(atob\\(\"(.*?)\"\\)")
         val base64Value = base64Pattern.find(responseText)?.groups?.get(1)?.value ?: ""
-        val decodedJson = base64Decode(base64Value)
-        val jsonObject = JSONObject(decodedJson)
+        val decodedString = customDecode(base64Value)
 
-        val domain = jsonObject.getString("domain")
-        val vidId = jsonObject.getString("id")
-        val videoUrls = mapOf(
-            "360p" to "https://$domain/$vidId",
-            "720p" to "https://$domain/www$vidId",
-            "1080p" to "https://$domain/whw$vidId"
-        )
-        val headers = mapOf(
-            "Sec-Fetch-Mode" to "cors"
-        )
-
-        for ((quality, link) in videoUrls) {
-            callback.invoke (
-                ExtractorLink (
-                    this.name,
-                    this.name,
-                    link,
-                    referer = mainUrl,
-                    getQualityFromName(quality),
-                    headers = headers
-                )
+        callback.invoke(
+            ExtractorLink(
+                this.name,
+                this.name,
+                decodedString,
+                referer = mainUrl,
+                Qualities.Unknown.value
             )
-        }
+        )
+        // val decodedJson = base64Decode(base64Value)
+        // val jsonObject = JSONObject(decodedJson)
+
+        // val domain = jsonObject.getString("domain")
+        // val vidId = jsonObject.getString("id")
+        // val videoUrls = mapOf(
+        //     "360p" to "https://$domain/$vidId",
+        //     "720p" to "https://$domain/www$vidId",
+        //     "1080p" to "https://$domain/whw$vidId"
+        // )
+        // val headers = mapOf(
+        //     "Sec-Fetch-Mode" to "cors"
+        // )
+
+        // for ((quality, link) in videoUrls) {
+        //     callback.invoke (
+        //         ExtractorLink (
+        //             this.name,
+        //             this.name,
+        //             link,
+        //             referer = mainUrl,
+        //             getQualityFromName(quality),
+        //             headers = headers
+        //         )
+        //     )
+        // }
     }
 }
