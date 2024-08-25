c44790dcc956e138ec847d172c1d8d0d1bb6d481
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-08-25 12:49:33 +0530

    try

--- Deadstream/src/main/kotlin/com/Deadstream/Extractor.kt
+++ Deadstream/src/main/kotlin/com/Deadstream/Extractor.kt
@@ -2,8 +2,8 @@ package com.Deadstream
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.extractors.Filemoon
+import com.lagradost.cloudstream3.extractors.Filesim
 
 class MyFileMoon : Filesim() {
-    override val mainUrl = "https://filemoon.nl"
+    override var mainUrl = "https://filemoon.nl"
 }
