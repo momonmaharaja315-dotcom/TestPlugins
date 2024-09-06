2452b8653491e150fcce6e9adaf182b5b5d1d8ed
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-09-06 12:21:28 +0530

    try

--- VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
+++ VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
@@ -249,41 +249,41 @@ open class VegaMoviesProvider : MainAPI() { // all providers must be an instance
     }
 
     data class Meta(
-        val id: String,
-        val imdb_id: String,
-        val type: String,
-        val poster: String,
-        val logo: String,
-        val background: String,
-        val moviedb_id: Int,
-        val name: String,
-        val description: String,
-        val genre: List<String>,
-        val releaseInfo: String,
-        val status: String,
-        val runtime: String,
-        val cast: List<String>,
-        val language: String,
-        val country: String,
-        val imdbRating: String,
-        val slug: String,
-        val year: String,
-        val videos: List<EpisodeDetails>
+        val id: String?,
+        val imdb_id: String?,
+        val type: String?,
+        val poster: String?,
+        val logo: String?,
+        val background: String?,
+        val moviedb_id: Int?,
+        val name: String?,
+        val description: String?,
+        val genre: List<String>?,
+        val releaseInfo: String?,
+        val status: String?,
+        val runtime: String?,
+        val cast: List<String>?,
+        val language: String?,
+        val country: String?,
+        val imdbRating: String?,
+        val slug: String?,
+        val year: String?,
+        val videos: List<EpisodeDetails>?
     )
 
     data class EpisodeDetails(
-        val id: String,
-        val name: String,
-        val season: Int,
-        val episode: Int,
-        val released: String,
-        val overview: String,
-        val thumbnail: String,
-        val moviedb_id: Int
+        val id: String?,
+        val name: String?,
+        val season: Int?,
+        val episode: Int?,
+        val released: String?,
+        val overview: String?,
+        val thumbnail: String?,
+        val moviedb_id: Int?
     )
 
     data class ResponseData(
-        val meta: Meta
+        val meta: Meta?
     )
 
     data class EpisodeLink(
