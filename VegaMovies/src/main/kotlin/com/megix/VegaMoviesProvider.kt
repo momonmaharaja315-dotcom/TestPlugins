aacd74f4b30f549dafae857cfd4bebc0849c8451
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-09-06 11:00:54 +0530

    try

---
+++ VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
@@ -0,0 +1,311 @@
+package com.megix
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import com.lagradost.cloudstream3.network.CloudflareKiller
+import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbUrl
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.google.gson.Gson
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+
+open class VegaMoviesProvider : MainAPI() { // all providers must be an instance of MainAPI
+    override var mainUrl = "https://vegamovies.pet"
+    override var name = "VegaMovies"
+    override val hasMainPage = true
+    override var lang = "hi"
+    override val hasDownloadSupport = true
+    val cinemeta_url = "https://v3-cinemeta.strem.io/meta"
+    private val cfInterceptor = CloudflareKiller()
+    override val supportedTypes = setOf(
+        TvType.Movie,
+        TvType.TvSeries
+    )
+
+    override val mainPage = mainPageOf(
+        "$mainUrl/page/%d/" to "Home",
+        "$mainUrl/category/featured/page/%d/" to "Amazon Prime",
+        "$mainUrl/category/web-series/netflix/page/%d/" to "Netflix",
+        "$mainUrl/category/web-series/disney-plus-hotstar/page/%d/" to "Disney Plus Hotstar",
+        "$mainUrl/category/web-series/amazon-prime-video/page/%d/" to "Amazon Prime",
+        "$mainUrl/category/web-series/mx-original/page/%d/" to "MX Original",
+        "$mainUrl/category/anime-series/page/%d/" to "Anime Series",
+        "$mainUrl/category/korean-series/page/%d/" to "Korean Series"
+    )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val document = app.get(request.data.format(page), interceptor = cfInterceptor).document
+        val home = document.select("article.post-item").mapNotNull { it.toSearchResult() }
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title = this.selectFirst("a")?.attr("title")?.replace("Download ", "").toString()
+        val href = fixUrl(this.selectFirst("a")?.attr("href").toString())
+        val imgTag = this.selectFirst("img.blog-picture")
+        var posterUrl = imgTag ?. attr("data-src")
+        if (posterUrl == null) {
+            posterUrl = fixUrlNull(this.selectFirst("img.blog-picture")?.attr("src").toString())
+        }
+
+        return newMovieSearchResponse(title, href, TvType.Movie) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchResponse = mutableListOf<SearchResponse>()
+        for (i in 1..3) {
+            val document = app.get("$mainUrl/page/$i/?s=$query", interceptor = cfInterceptor).document
+            val results = document.select("article.post-item").mapNotNull { it.toSearchResult() }
+            if (results.isEmpty()) {
+                break
+            }
+            searchResponse.addAll(results)
+        }
+        return searchResponse
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+        var title = document.selectFirst("meta[property=og:title]")?.attr("content")?.replace("Download ", "").toString()
+        val posterUrl = fixUrlNull(document.selectFirst("meta[property=og:image]")?.attr("content"))
+        val documentText = document.text()
+        val div = document.select("div.entry-content")
+        val hTagsDisc = div.select("h3:matches((?i)(SYNOPSIS|PLOT)), h4:matches((?i)(SYNOPSIS|PLOT))")
+        val pTagDisc = hTagsDisc.first()?.nextElementSibling()
+        var description = pTagDisc?.text()
+
+        val aTagRating = div.selectFirst("a:matches((?i)(Rating))")
+        val imdbUrl = aTagRating?.attr("href").toString()
+
+        val tvtype = if (url.contains("season") ||
+            title.contains("Season") ||
+            Regex("Series synopsis").containsMatchIn(documentText) || Regex("Series Name").containsMatchIn(documentText)) {
+            "series"
+        } else {
+            "movie"
+        }
+
+        val responseData = if (!imdbUrl.isNullOrEmpty()) {
+            val imdbId = imdbUrl.substringAfter("title/").substringBefore("/")
+            val jsonResponse = app.get("$cinemeta_url/$tvtype/$imdbId.json").text
+            if(jsonResponse.isNotEmpty() && jsonResponse.startsWith("{")) {
+                val gson = Gson()
+                gson.fromJson(jsonResponse, ResponseData::class.java)
+            }
+            else {
+                null
+            }
+        } else {
+            null
+        }
+
+        var cast: List<String> = emptyList()
+        var genre: List<String> = emptyList()
+        var imdbRating: String = ""
+        var year: String = ""
+        var background: String = posterUrl
+
+        if(responseData != null) {
+            description = if (responseData.meta.description.isNullOrEmpty()) {
+                description
+            } else {
+                responseData.meta.description
+            }
+
+            cast = responseData.meta.cast
+
+            title = if (responseData.meta.name.isNullOrEmpty()) {
+                title
+            } else {
+                responseData.meta.name
+            }
+
+            genre = responseData.meta.genre
+            imdbRating = responseData.meta.imdbRating
+            year = responseData.meta.year
+
+            posterUrl = if (responseData.meta.poster.isNullOrEmpty()) {
+                posterUrl
+            } else {
+                responseData.meta.poster
+            }
+
+            background = if (responseData.meta.background.isNullOrEmpty()) {
+                background
+            } else {
+                responseData.meta.background
+            }
+        }
+
+        if (tvtype == "series") {
+            val hTags = div.select("h3:matches((?i)(4K|[0-9]*0p)),h5:matches((?i)(4K|[0-9]*0p))")
+                .filter { element -> !element.text().contains("Zip", true) }
+
+            val tvSeriesEpisodes = mutableListOf<Episode>()
+            val episodesMap: MutableMap<Pair<Int, Int>, List<String>> = mutableMapOf()
+
+            for (tag in hTags) {
+                val realSeasonRegex = Regex("""(?:Season |S)(\d+)""")
+                val realSeason = realSeasonRegex.find(tag.toString())?.groupValues?.get(1) ?: "Unknown"
+
+                val pTag = tag.nextElementSibling()
+                val aTags: List<Element>? = if (pTag != null && pTag.tagName() == "p") {
+                    pTag.select("a")
+                } else {
+                    tag.select("a")
+                }
+
+                var unilink = aTags ?. find {
+                    it.text().contains("V-Cloud", ignoreCase = true) ||
+                    it.text().contains("Episode", ignoreCase = true) ||
+                    it.text().contains("Download", ignoreCase = true)
+                }
+
+                if (unilink == null) {
+                    unilink = aTags ?. find {
+                        it.text().contains("G-Direct", ignoreCase = true)
+                    }
+                }
+
+                val Eurl = unilink?.attr("href")
+                Eurl?.let { eurl ->
+                    val document2 = app.get(eurl).document
+                    val vcloudRegex = Regex("""https:\/\/vcloud\.lol\/[^\s"]+""")
+                    var vcloudLinks = vcloudRegex.findAll(document2.html()).mapNotNull { it.value }.toList()
+
+                    if (vcloudLinks.isEmpty()) {
+                        val fastDlRegex = Regex("""https:\/\/fastdl.icu\/embed\?download=[a-zA-Z0-9]+""")
+                        vcloudLinks = fastDlRegex.findAll(document2.html()).mapNotNull { it.value }.toList()
+                    }
+
+                    vcloudLinks.mapNotNull { vcloudlink ->
+                        val key = Pair(realSeason, vcloudLinks.indexOf(vcloudlink) + 1)
+                        if (episodesMap.containsKey(key)) {
+                            val currentList = episodesMap[key] ?: emptyList()
+                            val newList = currentList.toMutableList()
+                            newList.add(vcloudlink)
+                            episodesMap[key] = newList
+                        } else {
+                            episodesMap[key] = mutableListOf(vcloudlink)
+                        }
+                    }
+
+                    tvSeriesEpisodes.addAll(episodes)
+                    seasonNum++
+                }
+            }
+
+            for ((key, value) in episodesMap) {
+                val episodeInfo = responseData?.meta?.videos?.find { it.season == key.first && it.episode == key.second }
+                val data = value.map { source->
+                    EpisodeLink(
+                        source
+                    )
+                }
+                tvSeriesEpisodes.add(
+                    newEpisode(data) {
+                        this.name = episodeInfo?.name
+                        this.season = key.first
+                        this.episode = key.second
+                        this.posterUrl = episodeInfo?.thumbnail
+                        this.description = episodeInfo?.overview
+                    }
+                )
+            }
+
+            return newTvSeriesLoadResponse(title, url, TvType.TvSeries, tvSeriesEpisodes) {
+                this.posterUrl = posterUrl
+                this.plot = plot
+                this.rating = rating
+                this.seasonNames = seasonList.map { (name, int) -> SeasonData(int, name) }
+                addImdbUrl(imdbUrl)
+            }
+        } else {
+            val pTags = document.select("p:has(a:has(button))")
+            val data = pTags.mapNotNull { pTag ->
+                val link = pTag.selectFirst("a")?.attr("href") ?: ""
+                val doc = app.get(link).document
+                val source = doc.selectFirst("a:contains(V-Cloud)")?.attr("href").toString()
+                EpisodeLink(
+                    source
+                )
+            }
+            return newMovieLoadResponse(title, url, TvType.Movie, data) {
+                this.posterUrl = posterUrl
+                this.plot = description
+                this.tags = genre
+                this.rating = imdbRating?.toRatingInt()
+                this.year = year?.toIntOrNull()
+                this.backgroundPosterUrl = background
+                addActors(cast)
+                addImdbUrl(imdbUrl)
+            }
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val sources = parseJson<ArrayList<EpisodeLink>>(data)
+        sources.amap {
+            var source = it.source
+            if(source.contains("vcloud.lol/api")) {
+                val document = app.get(source).document
+                source = document.selectFirst("h4 > a")?.attr("href").toString()
+            }
+            loadExtractor(source, subtitleCallback, callback)
+        }
+        return true
+    }
+
+    data class Meta(
+        val id: String,
+        val imdb_id: String,
+        val type: String,
+        val poster: String,
+        val logo: String,
+        val background: String,
+        val moviedb_id: Int,
+        val name: String,
+        val description: String,
+        val genre: List<String>,
+        val releaseInfo: String,
+        val status: String,
+        val runtime: String,
+        val cast: List<String>,
+        val language: String,
+        val country: String,
+        val imdbRating: String,
+        val slug: String,
+        val year: String,
+        val videos: List<EpisodeDetails>
+    )
+
+    data class EpisodeDetails(
+        val id: String,
+        val name: String,
+        val season: Int,
+        val episode: Int,
+        val released: String,
+        val overview: String,
+        val thumbnail: String,
+        val moviedb_id: Int
+    )
+
+    data class ResponseData(
+        val meta: Meta
+    )
+
+    data class EpisodeLink(
+        val source: String
+    )
+}
