014b867481bbf8679799646c00baaf1877e80e87
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-09-07 17:50:07 +0530

    try

---
+++ Cinemaluxe/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 8
+
+cloudstream {
+    language = "hi"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Movies and Series upto 4K"
+     authors = listOf("megix")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://cinemaluxe.link/wp-content/uploads/2024/07/fresh-logo-dark-theme-.webp"
+}
