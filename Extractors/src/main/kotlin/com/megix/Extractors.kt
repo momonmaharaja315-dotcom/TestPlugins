a62f76d25c06e0e2cc8cac1c6a390f5fb65f846b
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-09-18 10:57:54 +0530

    try

--- Extractors/src/main/kotlin/com/megix/Extractors.kt
+++ Extractors/src/main/kotlin/com/megix/Extractors.kt
@@ -4,14 +4,80 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import okhttp3.FormBody
 
+open class VCloud : ExtractorApi() {
+    override val name: String = "V-Cloud"
+    override val mainUrl: String = "https://vcloud.lol"
+    override val requiresReferer = false
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val doc = app.get(url).document
+        val scriptTag = doc.selectFirst("script:containsData(url)") ?. data().toString()
+        val urlValue = Regex("var url = '([^']*)'").find(scriptTag) ?. groupValues ?. get(1) ?: ""
+        val document = app.get(urlValue).document
+
+        val size = document.selectFirst("i#size") ?. text()
+        val div = document.selectFirst("div.card-body")
+        val header = document.selectFirst("div.card-header") ?. text()
+        div?.select("a")?.amap {
+            val link = it.attr("href")
+            val text = it.text()
+            if (link.contains("pixeldra")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "Pixeldrain",
+                        "Pixeldrain $size",
+                        link,
+                        "",
+                        getIndexQuality(header),
+                    )
+                )
+            }
+            else if(text.contains("Download [Server : 10Gbps]")) {
+                val response = app.get(link, allowRedirects = false)
+                val downloadLink = response.headers["location"].toString().split("link=").getOrNull(1) ?: link
+                callback.invoke(
+                    ExtractorLink(
+                        "$name[Download]",
+                        "$name[Download] $size",
+                        downloadLink,
+                        "",
+                        getIndexQuality(header),
+                    )
+                )
+            }
+            else if(link.contains(".dev")) {
+                callback.invoke(
+                    ExtractorLink(
+                        "$name",
+                        "$name $size",
+                        link,
+                        "",
+                        getIndexQuality(header),
+                    )
+                )
+            }
+            else {
+                loadExtractor(link, subtitleCallback, callback)
+            }
+        }
+    }
+
+
+    private fun getIndexQuality(str: String?): Int {
+        return Regex("(\\d{3,4})[pP]").find(str ?: "") ?. groupValues ?. getOrNull(1) ?. toIntOrNull()
+            ?: Qualities.Unknown.value
+    }
+
+}
+
 class HubCloudClub : HubCloud() {
     override val name: String = "Hub-Cloud"
     override val mainUrl: String = "https://hubcloud.club"
-}
-
-class VCloud : HubCloud() {
-    override val name: String = "V-Cloud"
-    override val mainUrl: String = "https://vcloud.lol"
 }
 
 open class HubCloud : ExtractorApi() {
