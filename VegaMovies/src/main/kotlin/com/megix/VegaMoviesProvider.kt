e6823bd3e0606e63c92fb41ae45f6742e09df1ec
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-09-27 11:31:42 +0530

    try

--- VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
+++ VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import com.lagradost.cloudstream3.network.CloudflareKiller
-import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbUrl
+import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbUrlToIdNullable
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.google.gson.Gson
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
