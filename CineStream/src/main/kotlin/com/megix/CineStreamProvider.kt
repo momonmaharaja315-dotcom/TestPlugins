406ab29bad1de4496b3a349204d2f737c7949eb7
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-10-06 15:53:14 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineStreamProvider.kt
+++ CineStream/src/main/kotlin/com/megix/CineStreamProvider.kt
@@ -8,17 +8,22 @@ import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.argamap
 import com.lagradost.cloudstream3.network.CloudflareKiller
+import com.megix.CineStreamExtractors.invokeVegamovies
+import com.megix.CineStreamExtractors.invokeRogmovies
 
-class CineStreamProvider : MainAPI() {
+
+open class CineStreamProvider : MainAPI() {
     override var mainUrl = "https://cinemeta-catalogs.strem.io"
     override var name = "CineStream"
     override val hasMainPage = true
     override var lang = "en"
     override val hasDownloadSupport = true
     val cinemeta_url = "https://v3-cinemeta.strem.io"
-    val vegaMoviesAPI = "https://vegamovies.fans"
+    companion object {
+        val vegaMoviesAPI = "https://vegamovies.fans"
+        val rogMoviesAPI = "https://rogmovies.top"
+    }
     val wpRedisInterceptor by lazy { CloudflareKiller() }
-    val cfInterceptor = CloudflareKiller()
     override val supportedTypes = setOf(
         TvType.Movie,
         TvType.TvSeries,
@@ -141,11 +146,12 @@ class CineStreamProvider : MainAPI() {
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         val res = parseJson<LoadLinksData>(data)
+        val year = if(res.type == "movie") res.year.toIntOrNull() else res.year.substringBefore("-").toIntOrNull()
         argamap(
             {
                 invokeVegamovies(
                     res.title,
-                    res.year.toIntOrNull(),
+                    year,
                     res.season,
                     res.episode,
                     subtitleCallback,
@@ -244,146 +250,5 @@ class CineStreamProvider : MainAPI() {
         val imdbRating: String?,
         val popularity: Int?
     )
-
-
-    fun getEpisodeSlug(
-        season: Int? = null,
-        episode: Int? = null,
-    ): Pair<String, String> {
-        return if (season == null && episode == null) {
-            "" to ""
-        } else {
-            (if (season!! < 10) "0$season" else "$season") to (if (episode!! < 10) "0$episode" else "$episode")
-        }
-    }
-
-    fun getIndexQuality(str: String?): Int {
-        return Regex("(\\d{3,4})[pP]").find(str ?: "") ?. groupValues ?. getOrNull(1) ?. toIntOrNull()
-            ?: Qualities.Unknown.value
-    }
-
-    suspend fun loadCustomTagExtractor(
-        tag: String? = null,
-        url: String,
-        referer: String? = null,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit,
-        quality: Int? = null,
-    ) {
-        loadExtractor(url, referer, subtitleCallback) { link ->
-            callback.invoke(
-                ExtractorLink(
-                    link.source,
-                    "${link.name} $tag",
-                    link.url,
-                    link.referer,
-                    when (link.type) {
-                        ExtractorLinkType.M3U8 -> link.quality
-                        else -> quality ?: link.quality
-                    },
-                    link.type,
-                    link.headers,
-                    link.extractorData
-                )
-            )
-        }
-    }
-
-
-    suspend fun invokeVegamovies(
-        title: String? = null,
-        year: Int? = null,
-        season: Int? = null,
-        episode: Int? = null,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
-        invokeWpredis(
-            title,
-            year,
-            season,
-            episode,
-            subtitleCallback,
-            callback,
-            vegaMoviesAPI
-        )
-    }
-
-    private suspend fun invokeWpredis(
-        title: String? = null,
-        year: Int? = null,
-        season: Int? = null,
-        episode: Int? = null,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit,
-        api: String
-    ) {
-        val (seasonSlug, episodeSlug) = getEpisodeSlug(season, episode)
-        val cfInterceptor = CloudflareKiller()
-        val fixtitle = title?.substringBefore("-")?.substringBefore(":")?.replace("&", " ")
-        val url = if (season == null) {
-            "$api/search/$fixtitle $year"
-        } else {
-            "$api/search/$fixtitle season $season $year"
-        }
-        val domain= api.substringAfter("//").substringBefore(".")
-        app.get(url, interceptor = cfInterceptor).document.select("#main-content article")
-            .filter { element ->
-                element.text().contains(
-                    fixtitle.toString(), true
-                )
-            }
-            .amap {
-                val hrefpattern =
-                    Regex("""(?i)<a\s+href="([^"]+)"[^>]*?>[^<]*?\b($fixtitle)\b[^<]*?""").find(
-                        it.toString()
-                    )?.groupValues?.get(1)
-                if (hrefpattern!=null) {
-                    val res = hrefpattern.let { app.get(it).document }
-                    val hTag = if (season == null) "h5" else "h3,h5"
-                    val aTag =
-                        if (season == null) "Download Now" else "V-Cloud,Download Now,G-Direct"
-                    val sTag = if (season == null) "" else "(Season $season|S$seasonSlug)"
-                    val entries =
-                        res.select("div.entry-content > $hTag:matches((?i)$sTag.*(720p|1080p|2160p))")
-                            .filter { element ->
-                                !element.text().contains("Series", true) &&
-                                        !element.text().contains("Zip", true) &&
-                                        !element.text().contains("[Complete]", true) &&
-                                        !element.text().contains("480p, 720p, 1080p", true) &&
-                                        !element.text().contains(domain, true) &&
-                                        element.text().matches("(?i).*($sTag).*".toRegex())
-                            }
-                    entries.amap { it ->
-                        val tags =
-                            """(?:480p|720p|1080p|2160p)(.*)""".toRegex().find(it.text())?.groupValues?.get(1)
-                                ?.trim()
-                        val tagList = aTag.split(",")
-                        val href = it.nextElementSibling()?.select("a")?.find { anchor ->
-                            tagList.any { tag ->
-                                anchor.text().contains(tag.trim(), true)
-                            }
-                        }?.attr("href") ?: ""
-                        val selector =
-                            if (season == null) "p a:matches(V-Cloud|G-Direct)" else "h4:matches(0?$episode) ~ p a:matches(V-Cloud|G-Direct)"
-                        if (href.isNotEmpty()) {
-                            app.get(
-                                href, interceptor = wpRedisInterceptor
-                            ).document.select("div.entry-content > $selector").first()?.let { sources ->
-                                val server = sources.attr("href")
-                                loadCustomTagExtractor(
-                                    tags,
-                                    server,
-                                    "$api/",
-                                    subtitleCallback,
-                                    callback,
-                                    getIndexQuality(it.text())
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-    }
 }
 
