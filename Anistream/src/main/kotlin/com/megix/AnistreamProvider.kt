def0295e69637219f7785dbe930de6e1051bd07f
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-10-14 11:59:41 +0530

    try

--- Anistream/src/main/kotlin/com/megix/AnistreamProvider.kt
+++ Anistream/src/main/kotlin/com/megix/AnistreamProvider.kt
@@ -28,8 +28,8 @@ open class AnistreamProvider : MainAPI() {
 
         val animes = parseJson<AnimeResponse>(json)
         val home = animes.data.mapNotNull { anime ->
-            newMovieSearchResponse(anime.title, PassData(anime.malId).toJson(), TvType.Movie) {
-                //this.posterUrl = anime.images.jpg.imageUrl
+            newMovieSearchResponse(anime.title, PassData(anime.mal_id).toJson(), TvType.Movie) {
+                this.posterUrl = anime.images.jpg.image_url
             }
         }
         return newHomePageResponse(
@@ -46,8 +46,8 @@ open class AnistreamProvider : MainAPI() {
         val animeJson = app.get("$mainUrl/v4/anime?q=$query&sfw").text
         val animes = parseJson<AnimeResponse>(animeJson)
         animes.data.forEach { anime ->
-            searchResponse.add(newMovieSearchResponse(anime.title, PassData(anime.malId).toJson(), TvType.Movie) {
-                //this.posterUrl = anime.images.jpg.imageUrl
+            searchResponse.add(newMovieSearchResponse(anime.title, PassData(anime.mal_id).toJson(), TvType.Movie) {
+                this.posterUrl = anime.images.jpg.image_url
             })
         }
 
@@ -58,11 +58,11 @@ open class AnistreamProvider : MainAPI() {
 
     override suspend fun load(url: String): LoadResponse? {
         val anime = parseJson<PassData>(url)
-        val malId = anime.malId
-        val json = app.get("$mainUrl/anime/$malId").text
+        val mal_id = anime.mal_id
+        val json = app.get("$mainUrl/anime/$mal_id").text
         val animeData = parseJson<AnimeResponse>(json).data.first()
         val title = animeData.title
-        //val posterUrl = animeData.images.jpg.imageUrl
+        val posterUrl = animeData.images.jpg.image_url
         val rating = animeData.score
         val year = animeData.year
         val description = animeData.synopsis
@@ -70,11 +70,11 @@ open class AnistreamProvider : MainAPI() {
 
         val data = LoadLinksData(
             title,
-            malId,
+            mal_Id,
             year,
         ).toJson()
         return newMovieLoadResponse(title, url, TvType.Movie, data) {
-            //this.posterUrl = posterUrl
+            this.posterUrl = posterUrl
             this.plot = description
             this.year = year
         }
@@ -91,70 +91,34 @@ open class AnistreamProvider : MainAPI() {
     }
 
     data class PassData(
-        val malId: Int
+        val mal_id: Int
     )
 
     data class LoadLinksData(
         val title: String,
-        val malId: Int,
+        val mal_id: Int,
         val year: Int
     )
 
     data class AnimeResponse(
-        val data: List<AnimeData>,
-        val pagination: Pagination?
-    )
-
-    data class Pagination(
-        val lastVisiblePage: Int,
-        val hasNextPage: Boolean,
-        val currentPage: Int,
-        val items: Items
-    )
-
-    data class Items(
-        val count: Int,
-        val total: Int,
-        val perPage: Int
+        val data: List<AnimeData>
     )
 
     data class AnimeData(
-        val malId: Int,
+        val mal_id: Int,
         val url: String,
         val images: Images,
-        val trailer: Trailer,
-        val approved: Boolean,
-        val titles: List<Title>,
         val title: String,
-        val titleEnglish: String,
-        val titleJapanese: String,
-        val titleSynonyms: List<String>,
+        val title_english: String,
         val type: String,
-        val source: String,
-        val episodes: Int,
         val status: String,
         val airing: Boolean,
-        val aired: Aired,
         val duration: String,
-        val rating: String,
         val score: Double,
-        val scoredBy: Int,
-        val rank: Int,
-        val popularity: Int,
-        val members: Int,
-        val favorites: Int,
         val synopsis: String,
         val background: String,
-        val season: String,
         val year: Int,
-        val broadcast: Broadcast,
-        val producers: List<Producer>,
-        val licensors: List<Licensor>,
-        val studios: List<Studio>,
         val genres: List<Genre>,
-        val explicitGenres: List<Genre>,
-        val themes: List<Theme>,
-        val demographics: List<Demographic>
     )
 
     data class Images(
@@ -163,84 +127,13 @@ open class AnistreamProvider : MainAPI() {
     )
 
     data class Image(
-        val imageUrl: String,
-        val smallImageUrl: String,
-        val largeImageUrl: String
-    )
-
-    data class Trailer(
-        val youtubeId: String,
-        val url: String,
-        val embedUrl: String
-    )
-
-    data class Title(
-        val type: String,
-        val title: String
-    )
-
-    data class Aired(
-        val from: String,
-        val to: String,
-        val prop: Prop
-    )
-
-    data class Prop(
-        val from: Date,
-        val to: Date,
-        val string: String
-    )
-
-    data class Date(
-        val day: Int,
-        val month: Int,
-        val year: Int
-    )
-
-    data class Broadcast(
-        val day: String,
-        val time: String,
-        val timezone: String,
-        val string: String
-    )
-
-    data class Producer(
-        val malId: Int,
-        val type: String,
-        val name: String,
-        val url: String
-    )
-
-    data class Licensor(
-        val malId: Int,
-        val type: String,
-        val name: String,
-        val url: String
-    )
-
-    data class Studio(
-        val malId: Int,
-        val type: String,
-        val name: String,
-        val url: String
+        val image_url: String,
+        val small_image_url: String,
+        val large_image_url: String
     )
 
     data class Genre(
-        val malId: Int,
-        val type: String,
-        val name: String,
-        val url: String
-    )
-
-    data class Theme(
-        val malId: Int,
-        val type: String,
-        val name: String,
-        val url: String
-    )
-
-    data class Demographic(
-        val malId: Int,
+        val mal_id: Int,
         val type: String,
         val name: String,
         val url: String
