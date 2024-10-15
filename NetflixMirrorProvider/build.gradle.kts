d8819acd9f6b17e674edd3637d6b9b4fac8359c5
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-10-15 09:29:17 +0530

    try

---
+++ NetflixMirrorProvider/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 7
+
+cloudstream {
+    //language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Netflix, PrimeVideo Content in Multiple Languages"
+    authors = listOf("Horis, megix")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+
+    iconUrl = "https://iosmirror.cc/img/nf2/icon_x192.png"
+}
--- NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/NetflixMirrorPlugin.kt
+++ NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/NetflixMirrorPlugin.kt
@@ -1,14 +1,15 @@
-package com.megix
+package com.horis.cloudstreamplugins
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class Anistream: Plugin() {
+class NetflixMirrorPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(AnistreamProvider())
-        //registerExtractorAPI(Chillx())
+        registerMainAPI(NetflixMirrorProvider())
+        registerMainAPI(PrimeVideoMirrorProvider())
     }
+
 }
