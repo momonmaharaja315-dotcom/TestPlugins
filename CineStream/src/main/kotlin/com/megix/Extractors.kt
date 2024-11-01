4ddca3f76ac8335c9778abc8edd2e622f8c785f7
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-11-01 14:41:47 +0530

    try

--- CineStream/src/main/kotlin/com/megix/Extractors.kt
+++ CineStream/src/main/kotlin/com/megix/Extractors.kt
@@ -1,5 +1,7 @@
+package com.megix
+
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.
+import com.lagradost.cloudstream3.utils.*
 import com.megix.CineStreamProvider.Companion.animepaheAPI
 
 class Kwik : ExtractorApi() {
