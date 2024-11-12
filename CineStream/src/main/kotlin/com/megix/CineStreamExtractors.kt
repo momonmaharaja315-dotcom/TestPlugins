2210efc27712a858688332dd515bf1fe58013c26
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-11-12 10:27:51 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineStreamExtractors.kt
+++ CineStream/src/main/kotlin/com/megix/CineStreamExtractors.kt
@@ -20,7 +20,7 @@ object CineStreamExtractors : CineStreamProvider() {
 
     suspend fun getSHA256ofJSON(input: String): String {
         val digest = MessageDigest.getInstance("SHA-256")
-        val hashBytes = digest.digest(input.toByteArray())
+        val hashBytes = digest.digest(input.toByteArray(Charsets.UTF_8))
         return hashBytes.joinToString("") { "%02x".format(it) }
     }
 
