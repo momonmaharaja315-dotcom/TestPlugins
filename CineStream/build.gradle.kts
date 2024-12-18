4108b4ba3a5d044eaecbbc0f1cf75f0e794c89fa
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-12-18 11:02:47 +0530

    try

--- CineStream/build.gradle.kts
+++ CineStream/build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.konan.properties.Properties
 
-version = 43
+version = 44
 
 android {
     defaultConfig {
         val properties = Properties()
         properties.load(project.rootProject.file("local.properties").inputStream())
-        buildConfigField("String", "CONSUMET_API", "\"${properties.getProperty("CONSUMET_API")}\"")
         buildConfigField("String", "WHVX_TOKEN", "\"${properties.getProperty("WHVX_TOKEN")}\"")
     }
 }
