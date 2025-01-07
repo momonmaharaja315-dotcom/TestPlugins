5b3343937ecd4ede5cadf541b3cd5677274b6f5f
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-01-07 19:23:29 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineStreamProvider.kt
+++ CineStream/src/main/kotlin/com/megix/CineStreamProvider.kt
@@ -128,9 +128,9 @@ open class CineStreamProvider : MainAPI() {
         val newRequestData = request.data.replace("###", skip.toString())
         val json = app.get("$newRequestData.json").text
         val movies = tryParseJson<Home>(json)
-        val movieCount = movies?.metas.size
+        val movieCount = movies?.metas?.size
         skipMap[request.name] = skip + movieCount
-        val home = movies?.metas.mapNotNull { movie ->
+        val home = movies?.metas?.mapNotNull { movie ->
             if (movie.id.startsWith("mf")) {
                 null
             }
