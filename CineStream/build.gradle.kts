ddce0d2180ac408421015e1a11b691f69b75636a
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-01-23 12:42:39 +0530

    try

---
+++ CineStream/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+version = 61
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        android.buildFeatures.buildConfig=true
+        buildConfigField("String", "WHVX_TOKEN", "\"${properties.getProperty("WHVX_TOKEN")}\"")
+        buildConfigField("String", "CONSUMET_API", "\"${properties.getProperty("CONSUMET_API")}\"")
+    }
+}
+
+cloudstream {
+    //language = "en"
+    description = "Multi API Extension"
+    authors = listOf("megix")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama",
+        "Anime",
+        "Torrent",
+    )
+
+    iconUrl = "https://us.123rf.com/450wm/mrshamsjaman/mrshamsjaman2008/mrshamsjaman200800943/154338064-initial-letter-cs-logo-or-sc-logo-vector-design-template.jpg"
+}
