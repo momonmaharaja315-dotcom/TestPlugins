017145765672d4701403b6d564c3de206f488dee
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-03-22 10:38:18 +0530

    try

--- Perverzija/src/main/kotlin/com/CXXX/Extractors.kt
+++ Perverzija/src/main/kotlin/com/CXXX/Extractors.kt
@@ -6,57 +6,64 @@ import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.M3u8Helper
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.network.CloudflareKiller
 
 open class Xtremestream : ExtractorApi() {
     override var name = "Xtremestream"
     override var mainUrl = "https://pervl4.xtremestream.xyz"
-    override val requiresReferer = false
-    private val cfInterceptor = CloudflareKiller()
+    override val requiresReferer = true
+    private val client = OkHttpClient()
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit){
 
-        val response = app.get(
-            url, referer = referer , headers = mapOf(
-                "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                "Referer" to "$referer",
-                "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-            ),
-            interceptor = cfInterceptor
-        )
+        // val response = app.get(
+        //     url, referer = referer , headers = mapOf(
+        //         "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        //         "Referer" to "$referer",
+        //         "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+        //     )
+        // )
+
+        val request = Request.Builder()
+            .url(url)
+            .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+            .header("Referer", referer.toString())
+            .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+            .build()
+
+        val response = client.newCall(request).execute()
 
         callback.invoke(
             ExtractorLink(
                 "Xtremestream",
                 "Xtremestream",
-                response.text,
+                response.body?.string() ?: "Empty",
                 "",
                 Qualities.Unknown.value,
             )
         )
 
-        val playerScript =
-            response.document.selectXpath("//script[contains(text(),'var video_id')]")
-                .html()
-        val sources = mutableListOf<ExtractorLink>()
-        if (playerScript.isNotBlank()) {
-            val videoId = playerScript.substringAfter("var video_id = `").substringBefore("`;")
-            val m3u8LoaderUrl =
-                playerScript.substringAfter("var m3u8_loader_url = `").substringBefore("`;")
+        // val playerScript =
+        //     response.document.selectXpath("//script[contains(text(),'var video_id')]")
+        //         .html()
+        // val sources = mutableListOf<ExtractorLink>()
+        // if (playerScript.isNotBlank()) {
+        //     val videoId = playerScript.substringAfter("var video_id = `").substringBefore("`;")
+        //     val m3u8LoaderUrl =
+        //         playerScript.substringAfter("var m3u8_loader_url = `").substringBefore("`;")
 
-            if (videoId.isNotBlank() && m3u8LoaderUrl.isNotBlank()) {
-                M3u8Helper.generateM3u8(
-                    name,
-                    "$m3u8LoaderUrl/$videoId",
-                    "$m3u8LoaderUrl/$videoId"
-                ).forEach { link ->
-                    sources.add(link)
-                }
-            }
-        }
-        sources.forEach { source ->
-            callback.invoke(source)
-        }
+        //     if (videoId.isNotBlank() && m3u8LoaderUrl.isNotBlank()) {
+        //         M3u8Helper.generateM3u8(
+        //             name,
+        //             "$m3u8LoaderUrl/$videoId",
+        //             "$m3u8LoaderUrl/$videoId"
+        //         ).forEach { link ->
+        //             sources.add(link)
+        //         }
+        //     }
+        // }
+        // sources.forEach { source ->
+        //     callback.invoke(source)
+        // }
 
     }
 }
