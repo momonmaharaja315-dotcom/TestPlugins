96ef690620d237da50ba64c923a5c6be318c04bf
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-03-22 11:15:00 +0530

    try

--- Perverzija/src/main/kotlin/com/CXXX/Extractors.kt
+++ Perverzija/src/main/kotlin/com/CXXX/Extractors.kt
@@ -8,6 +8,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jsoup.Jsoup
 
 open class Xtremestream : ExtractorApi() {
     override var name = "Xtremestream"
@@ -17,14 +18,6 @@ open class Xtremestream : ExtractorApi() {
 
     override suspend fun getUrl(url: String, referer: String?, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit){
 
-        // val response = app.get(
-        //     url, referer = referer , headers = mapOf(
-        //         "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-        //         "Referer" to "$referer",
-        //         "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-        //     )
-        // )
-
         val request = Request.Builder()
             .url(url)
             .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
@@ -33,39 +26,29 @@ open class Xtremestream : ExtractorApi() {
             .build()
 
         val response = client.newCall(request).execute()
+        val html = response.body?.string() ?: return
 
-        callback.invoke(
-            ExtractorLink(
-                "Xtremestream",
-                "Xtremestream",
-                response.body?.string() ?: "Empty",
-                "",
-                Qualities.Unknown.value,
-            )
-        )
+        val playerScript =
+            Jsoup.parse(html).selectXpath("//script[contains(text(),'var video_id')]")
+                .html()
+        val sources = mutableListOf<ExtractorLink>()
+        if (playerScript.isNotBlank()) {
+            val videoId = playerScript.substringAfter("var video_id = `").substringBefore("`;")
+            val m3u8LoaderUrl =
+                playerScript.substringAfter("var m3u8_loader_url = `").substringBefore("`;")
 
-        // val playerScript =
-        //     response.document.selectXpath("//script[contains(text(),'var video_id')]")
-        //         .html()
-        // val sources = mutableListOf<ExtractorLink>()
-        // if (playerScript.isNotBlank()) {
-        //     val videoId = playerScript.substringAfter("var video_id = `").substringBefore("`;")
-        //     val m3u8LoaderUrl =
-        //         playerScript.substringAfter("var m3u8_loader_url = `").substringBefore("`;")
-
-        //     if (videoId.isNotBlank() && m3u8LoaderUrl.isNotBlank()) {
-        //         M3u8Helper.generateM3u8(
-        //             name,
-        //             "$m3u8LoaderUrl/$videoId",
-        //             "$m3u8LoaderUrl/$videoId"
-        //         ).forEach { link ->
-        //             sources.add(link)
-        //         }
-        //     }
-        // }
-        // sources.forEach { source ->
-        //     callback.invoke(source)
-        // }
-
+            if (videoId.isNotBlank() && m3u8LoaderUrl.isNotBlank()) {
+                M3u8Helper.generateM3u8(
+                    name,
+                    "$m3u8LoaderUrl/$videoId",
+                    "$m3u8LoaderUrl/$videoId"
+                ).forEach { link ->
+                    sources.add(link)
+                }
+            }
+        }
+        sources.forEach { source ->
+            callback.invoke(source)
+        }
     }
 }
