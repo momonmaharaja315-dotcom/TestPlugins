35c17c5e36821d0b1dd00d57f5b198c943cefc2b
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-03-22 10:28:29 +0530

    try

--- Perverzija/src/main/kotlin/com/CXXX/Extractors.kt
+++ Perverzija/src/main/kotlin/com/CXXX/Extractors.kt
@@ -19,7 +19,7 @@ open class Xtremestream : ExtractorApi() {
         val response = app.get(
             url, referer = referer , headers = mapOf(
                 "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                "Referer" to referer,
+                "Referer" to "$referer",
                 "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
             ),
             interceptor = cfInterceptor
