0ae70e947a1c301260c9a7c8a3625e07c50bb14d
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-04-13 09:21:23 +0530

    try

--- CineStream/src/main/kotlin/com/megix/TraktProvider.kt
+++ CineStream/src/main/kotlin/com/megix/TraktProvider.kt
@@ -2,6 +2,8 @@ package com.megix
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.metaproviders.TraktProvider
+
 
 class CineTraktProvider : TraktProvider() {
     override var name = "Trakt(CineStream)"
