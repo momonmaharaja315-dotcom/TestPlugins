1ee8c221cbe8333f0c714d9b33ef60ec4cb62f7a
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-05-26 14:45:51 +0530

    try

---
+++ CineStream/src/main/kotlin/com/megix/CineStream.kt
@@ -0,0 +1,29 @@
+package com.megix
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+open class CineStream: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(CineStreamProvider())
+        registerExtractorAPI(Kwik())
+        registerExtractorAPI(Animezia())
+        registerExtractorAPI(server2())
+        registerExtractorAPI(MultimoviesAIO())
+        registerExtractorAPI(Strwishcom())
+        registerExtractorAPI(CdnwishCom())
+        registerExtractorAPI(Dhcplay())
+        registerExtractorAPI(Asnwish())
+        registerExtractorAPI(Multimovies())
+        registerExtractorAPI(Pahe())
+        registerExtractorAPI(Smoothpre())
+        registerExtractorAPI(Ryderjet())
+        registerExtractorAPI(SuperVideo())
+        registerExtractorAPI(MixDropPs())
+        registerExtractorAPI(Dlions())
+        registerExtractorAPI(Watchadsontape())
+        registerExtractorAPI(MixDropSi())
+    }
+}
