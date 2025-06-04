9be58f06d924dfe2fba6213e04ae71a0e1434d11
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-06-04 14:36:35 +0530

    try

--- Simkl/src/main/kotlin/com/megix/SimklProvider.kt
+++ Simkl/src/main/kotlin/com/megix/SimklProvider.kt
@@ -2,9 +2,9 @@ package com.megix
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.syncproviders.providers.SimklApi.Companion.getPosterUrl
+import com.lagradost.cloudstream3.LoadResponse.Companion.addSimklId
 
 class SimklProvider: MainAPI() {
     override var name = "Simkl"
@@ -32,7 +32,7 @@ class SimklProvider: MainAPI() {
         val jsonString = app.get("$apiUrl/search/movie?q=$query&client_id=$auth").text
         val json = parseJson<Array<SimklResponse>>(jsonString)
         val data = json.map {
-            newMovieSearchResponse("${it.title}", "$mainUrl/${it.ids?.simkl_id}") {
+            newMovieSearchResponse(it.title, "$mainUrl/${it.ids.simkl_id}") {
                 this.posterUrl = getPosterUrl(it.poster.toString())
             }
         }
@@ -43,7 +43,7 @@ class SimklProvider: MainAPI() {
         val jsonString = app.get(apiUrl + request.data + page).text
         val json = parseJson<Array<SimklResponse>>(jsonString)
         val data = json.map {
-            newMovieSearchResponse("${it.title}", "$mainUrl/${it.ids?.simkl_id}") {
+            newMovieSearchResponse("it.title", "$mainUrl/${it.ids.simkl_id}") {
                 this.posterUrl = getPosterUrl(it.poster.toString())
             }
         }
@@ -58,8 +58,26 @@ class SimklProvider: MainAPI() {
     }
 
     override suspend fun load(url: String): LoadResponse {
-        val id = url.substringAfterLast("/")
-        return newMovieLoadResponse(id, url, TvType.Movie, url) {
+        val simklId = url.substringAfterLast("/")
+        val jsonString = app.get("$apiUrl/movies/$simklId?extended=full&client_id=$auth").text
+        val json = parseJson<Array<SimklResponse>>(jsonString)
+        val title = json.title
+        val poster = json.poster
+        val type = json.type
+        val year = json.year
+        val overview = json.overview
+        val genres = json.genres?.map { it.toString() }
+        val country = json.country
+        val imdbId = json.ids.imdb
+        val tmdbId = json.ids.tmdb
+        //val recommendations = json.users_recommendations?.map { it.toSearchResponse() }
+        return newMovieLoadResponse(json.title, url, TvType.Movie, url) {
+            this.posterUrl = json.poster
+            this.plot = json.overview
+            this.tags = genres
+            this.rating = json.ratings.simkl.rating
+            this.year = json.year
+            this.addSimklId(simklId)
         }
     }
 
@@ -80,42 +98,49 @@ class SimklProvider: MainAPI() {
     }
 
     data class SimklResponse (
-        var title          : String?  = null,
+        var title          : String,
+        var year           : Int?     = null,
+        var type           : String?  = null,
         var url            : String?  = null,
         var poster         : String?  = null,
         var fanart         : String?  = null,
-        var ids            : Ids?     = Ids(),
+        var ids            : Ids      = Ids(),
         var release_date   : String?  = null,
-        var rank           : Int?     = null,
-        var drop_rate      : String?  = null,
-        var watched        : Int?     = null,
-        var plan_to_watch  : Int?     = null,
-        var ratings        : Ratings? = Ratings(),
+        var ratings        : Ratings = Ratings(),
         var country        : String?  = null,
+        var certification  : String?  = null,
         var runtime        : String?  = null,
         var status         : String?  = null,
         var total_episodes : Int?     = null,
         var network        : String?  = null,
-        var overview       : String?  = null
+        var overview       : String?  = null,
+        var genres         : ArrayList<String> = arrayListOf(),
+        var country        : String?  = null,
+        var runtime        : Int?     = null,
+        var users_recommendations : ArrayList<UsersRecommendations> = arrayListOf()
     )
 
     data class Ids (
-        var simkl_id : Int?    = null,
+        var simkl_id : Int,
+        var tmdb     : String? = null,
+        var imdb     : String? = null,
         var slug     : String? = null
     )
 
     data class Ratings (
-        var simkl : Simkl? = Simkl(),
-        var imdb  : Imdb?  = Imdb()
+        var simkl : Simkl = Simkl(),
     )
 
     data class Simkl (
-        var rating : Double? = null,
-        var votes  : Int?    = null
+        var rating : Double,
+        var votes  : Int
     )
 
-    data class Imdb (
-        var rating : Double? = null,
-        var votes  : Int?    = null
+    data class UsersRecommendations (
+        var title  : String,
+        var year   : Int?    = null,
+        var poster : String? = null,
+        var type   : String? = null,
+        var ids    : Ids   = Ids()
     )
 }
