ce582b7339f9d84885174cfca284803a5012f847
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-06-05 11:46:15 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineSimklProvider.kt
+++ CineStream/src/main/kotlin/com/megix/CineSimklProvider.kt
@@ -4,7 +4,6 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
-import com.lagradost.cloudstream3.syncproviders.providers.SimklApi.Companion.getPosterUrl
 import com.lagradost.cloudstream3.LoadResponse.Companion.addSimklId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
@@ -12,16 +11,55 @@ import com.lagradost.cloudstream3.runAllAsync
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import com.megix.CineStreamExtractors.invokeVegamovies
+import com.megix.CineStreamExtractors.invokeMoviesmod
+import com.megix.CineStreamExtractors.invokeTopMovies
+import com.megix.CineStreamExtractors.invokeMoviesdrive
+import com.megix.CineStreamExtractors.invokeW4U
+import com.megix.CineStreamExtractors.invokeWYZIESubs
+import com.megix.CineStreamExtractors.invokeAnizone
+// import com.megix.CineStreamExtractors.invokeVidbinge
+import com.megix.CineStreamExtractors.invokeUhdmovies
+// import com.megix.CineStreamExtractors.invokeRar
+import com.megix.CineStreamExtractors.invokeAnimes
+import com.megix.CineStreamExtractors.invokeMultimovies
+import com.megix.CineStreamExtractors.invokeStreamify
+import com.megix.CineStreamExtractors.invokeCinemaluxe
+import com.megix.CineStreamExtractors.invokeBollyflix
+import com.megix.CineStreamExtractors.invokeTorrentio
+import com.megix.CineStreamExtractors.invokeTokyoInsider
+import com.megix.CineStreamExtractors.invokeTvStream
+import com.megix.CineStreamExtractors.invokeAllanime
+import com.megix.CineStreamExtractors.invokeStreamAsia
+import com.megix.CineStreamExtractors.invokeNetflix
+import com.megix.CineStreamExtractors.invokePrimeVideo
+import com.megix.CineStreamExtractors.invokeFlixhq
+import com.megix.CineStreamExtractors.invokeSkymovies
+import com.megix.CineStreamExtractors.invokeMoviesflix
+import com.megix.CineStreamExtractors.invokeHdmovie2
+import com.megix.CineStreamExtractors.invokeHindmoviez
+import com.megix.CineStreamExtractors.invokeMostraguarda
+import com.megix.CineStreamExtractors.invokePlayer4U
+import com.megix.CineStreamExtractors.invokePrimeWire
+import com.megix.CineStreamExtractors.invokeProtonmovies
+import com.megix.CineStreamExtractors.invokeThepiratebay
+import com.megix.CineStreamExtractors.invokeTom
+import com.megix.CineStreamExtractors.invokeAllmovieland
+import com.megix.CineStreamExtractors.invoke4khdhub
+// import com.megix.CineStreamExtractors.invokeVidJoy
+import com.megix.CineStreamExtractors.invokeMovies4u
+import com.megix.CineStreamExtractors.invokeSoaper
+import com.megix.CineStreamExtractors.invokeAsiaflix
+import com.megix.CineStreamExtractors.invoke2embed
+import com.megix.CineStreamExtractors.invokePrimebox
+import com.megix.CineStreamExtractors.invokePrimenet
 import com.megix.CineStreamExtractors.invokeAnimeparadise
 import com.megix.CineStreamExtractors.invokeGojo
 import com.megix.CineStreamExtractors.invokeSudatchi
-import com.megix.CineStreamExtractors.invokeAnimes
-import com.megix.CineStreamExtractors.invokeAnizone
-import com.megix.CineStreamExtractors.invokeTorrentio
-import com.megix.CineStreamExtractors.invokeAllanime
+import com.megix.CineStreamExtractors.invokePhoenix
 
 class CineSimklProvider: MainAPI() {
-    override var name = "Simkl"
+    override var name = "CineSimkl"
     override var mainUrl = "https://simkl.com"
     override var supportedTypes = setOf(
         TvType.Movie,
@@ -43,9 +81,25 @@ class CineSimklProvider: MainAPI() {
         "/anime/trending/?extended=overview,metadata,tmdb,genres,trailer&client_id=$auth&limit=$mediaLimit&page=" to "Trending Anime",
     )
 
-    fun getSimklId(url: String): String {
+    private fun getSimklId(url: String): String {
         val regex = Regex("""simkl\.com\/(?:anime|movies|tv|movie)\/(\d+)""")
         return regex.find(url)?.groupValues?.get(1) ?: ""
+    }
+
+    private fun getPosterUrl(
+        url: String? = null,
+        type: String,
+     ): String? {
+        if(url == null) {
+            return null
+        } else if(type == "episode") {
+            return "https://simkl.in/episodes/${url}_c.webp"
+        } else if(type == "poster") {
+            return "https://simkl.in/posters/${url}_m.webp"
+        }
+        else {
+            return "https://simkl.in/fanart/${url}_medium.webp"
+        }
     }
 
     override suspend fun search(query: String): List<SearchResponse> = coroutineScope {
@@ -54,8 +108,8 @@ class CineSimklProvider: MainAPI() {
             val result = runCatching {
                 val json = app.get("$apiUrl/search/$type?q=$query&client_id=$auth").text
                 parseJson<Array<SimklResponse>>(json).map {
-                    newMovieSearchResponse("${it.title}", "$mainUrl/${it.endpoint_type}/${it.ids?.simkl}/${it.ids?.slug}") {
-                        posterUrl = getPosterUrl(it.poster.toString())
+                    newMovieSearchResponse("${it.title}", "$mainUrl/${it.endpoint_type}/${it.ids?.simkl_id}/${it.ids?.slug}") {
+                        posterUrl = getPosterUrl(it.poster, "poster")
                     }
                 }
             }.getOrDefault(emptyList())
@@ -85,7 +139,7 @@ class CineSimklProvider: MainAPI() {
         val json = parseJson<Array<SimklResponse>>(jsonString)
         val data = json.map {
             newMovieSearchResponse("${it.title}", "$mainUrl${it.url}") {
-                this.posterUrl = getPosterUrl(it.poster.toString())
+                this.posterUrl = getPosterUrl(it.poster, "poster")
             }
         }
 
@@ -104,12 +158,17 @@ class CineSimklProvider: MainAPI() {
         val json = parseJson<SimklResponse>(jsonString)
         val genres = json.genres?.map { it.toString() }
         val country = json.country
-        val tvType = json.type.toString()
+        val tvType = json.type ?: ""
+        val en_title = json.en_title ?: json.title
+        val country = json.country ?: ""
+        val isAnime = if(tvtype == "anime") true else false
+        val isBollywood = if(country == "IN") true else false
+        val isAsian = if(!isAnime && (country == "JP" || country == "KR" || country == "CN")) true else false
 
         if (tvType == "movie" || (tvType == "anime" && json.anime_type?.equals("movie") == true)) {
             val data = LoadLinksData(
                 json.title,
-                json.en_title,
+                en_title,
                 tvType,
                 simklId?.toIntOrNull(),
                 json.ids?.imdb,
@@ -119,12 +178,17 @@ class CineSimklProvider: MainAPI() {
                 json.ids?.mal?.toIntOrNull(),
                 null,
                 null,
+                null,
+                isAnime,
+                isBollywood,
+                isAsian
             ).toJson()
             return newMovieLoadResponse("${json.en_title ?: json.title}", url, TvType.Movie, data) {
-                this.posterUrl = getPosterUrl(json.poster.toString())
-                this.backgroundPosterUrl = "https://simkl.in/fanart/${json.fanart}_medium.webp"
+                this.posterUrl = getPosterUrl(json.poster, "poster")
+                this.backgroundPosterUrl = getPosterUrl(json.fanart, "fanart") ?: getPosterUrl(json.poster, "poster")
                 this.plot = json.overview
                 this.tags = genres
+                this.duration = json.runtime?.toIntOrNull()
                 this.rating = json.ratings?.simkl?.rating.toString().toRatingInt()
                 this.year = json.year
                 this.contentRating = json.certification
@@ -138,7 +202,7 @@ class CineSimklProvider: MainAPI() {
                 newEpisode(
                     LoadLinksData(
                         json.title,
-                        json.en_title,
+                        en_title,
                         tvType,
                         simklId?.toIntOrNull(),
                         json.ids?.imdb,
@@ -148,20 +212,25 @@ class CineSimklProvider: MainAPI() {
                         json.ids?.mal?.toIntOrNull(),
                         json.season?.toIntOrNull() ?: it.season,
                         it.episode,
+                        it.date.toString().substringBefore("-").toIntOrNull(),
+                        isAnime,
+                        isBollywood,
+                        isAsian
                     ).toJson()
                 ) {
                     this.season = it.season
                     this.episode = it.episode
-                    this.posterUrl = "https://simkl.in/episodes/${it.img}_c.webp"
+                    this.posterUrl = getPosterUrl(it.poster, "episode")
                     addDate(it.date)
                 }
             }
 
             return newTvSeriesLoadResponse("${json.en_title ?: json.title}", url, TvType.TvSeries, episodes) {
-                this.posterUrl = getPosterUrl(json.poster.toString())
-                this.backgroundPosterUrl = "https://simkl.in/fanart/${json.fanart}_medium.webp"
+                this.posterUrl = getPosterUrl(json.poster)
+                this.backgroundPosterUrl = getPosterUrl(json.fanart, "fanart") ?: getPosterUrl(json.poster, "poster")
                 this.plot = json.overview
                 this.tags = genres
+                this.duration = json.runtime?.toIntOrNull()
                 this.rating = json.ratings?.simkl?.rating.toString().toRatingInt()
                 this.year = json.year
                 this.contentRating = json.certification
@@ -178,17 +247,56 @@ class CineSimklProvider: MainAPI() {
             callback: (ExtractorLink) -> Unit
     ): Boolean {
         val res = parseJson<LoadLinksData>(data)
-        if(res.tvtype?.equals("anime") == true) {
-            runAllAsync(
-                { invokeAnimes(res.malId, res.anilistId, res.episode, res.year, "kitsu", subtitleCallback, callback) },
-                { invokeSudatchi(res.anilistId, res.episode, subtitleCallback, callback) },
-                { invokeGojo(res.anilistId, res.episode, callback) },
-                { invokeAnimeparadise(res.title, res.malId, res.episode, subtitleCallback, callback) },
-                { invokeAllanime(res.title, res.year, res.episode, subtitleCallback, callback) },
-                { invokeAnizone(res.title, res.episode, subtitleCallback, callback) },
-                { invokeTorrentio(res.imdbId, res.season, res.episode, callback) },
-            )
-        }
+
+        runAllAsync(
+            { if(res.isAnime) invokeAnimes(res.malId, res.anilistId, res.episode, res.year, "kitsu", subtitleCallback, callback) },
+            { if(res.isAnime) invokeSudatchi(res.anilistId, res.episode, subtitleCallback, callback) },
+            { if(res.isAnime) invokeGojo(res.anilistId, res.episode, callback) },
+            { if(res.isAnime) invokeAnimeparadise(res.title, res.malId, res.episode, subtitleCallback, callback) },
+            { if(res.isAnime) invokeAllanime(res.title, res.year, res.episode, subtitleCallback, callback) },
+            { if(res.isAnime) invokeAnizone(res.title, res.episode, subtitleCallback, callback) },
+            { if(res.isAnime) invokeTokyoInsider(res.title, res.episode, subtitleCallback, callback) },
+            { invokeTorrentio(res.imdbId, res.season, res.episode, callback) },
+            { if (!res.isBollywood) invokeVegamovies("VegaMovies", res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { if (res.isBollywood) invokeVegamovies("RogMovies", res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { invokeNetflix(res.en_title, res.year, res.season, res.episode, subtitleCallback, callback) },
+            { invokePrimeVideo(res.en_title, res.year, res.season, res.episode, subtitleCallback, callback) },
+            { if (res.season == null) invokeStreamify(res.id, callback) },
+            //{ invokeMultimovies(multimoviesAPI, res.title, res.season, res.episode, subtitleCallback, callback) },
+            { if (res.isBollywood) invokeTopMovies(res.title, res.year, res.season, res.episode, subtitleCallback, callback) },
+            { if (!res.isBollywood) invokeMoviesmod(res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { if (res.isAsian && res.season != null) invokeStreamAsia(res.title, "kdhd", res.season, res.episode, subtitleCallback, callback) },
+            { invokeMoviesdrive(res.en_title, res.imdbId ,res.season, res.episode, subtitleCallback, callback) },
+            { if(!res.isAnime) invokeAsiaflix(res.title, res.season, res.episode, res.airedYear, subtitleCallback, callback) },
+            { invokeCinemaluxe(res.en_title, res.year, res.season, res.episode, callback, subtitleCallback) },
+            { if (!res.isAnime) invokeSkymovies(res.title, res.airedYear, res.episode, subtitleCallback, callback) },
+            { if (!res.isAnime) invokeHdmovie2(res.title, res.airedYear, res.episode, subtitleCallback, callback) },
+            { if (!res.isAnime) invokeFlixhq(res.title, res.season, res.episode, subtitleCallback, callback) },
+            { invokeBollyflix(res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { invokeMovies4u(res.imdbId, res.en_title, res.year, res.season, res.episode, subtitleCallback, callback) },
+            { invokeTorrentio(res.imdbId, res.season, res.episode, callback) },
+            //{ if (!res.isBollywood) invokeHindmoviez("HindMoviez", hindMoviezAPI, res.id, res.season, res.episode, callback) },
+            // { if (res.isBollywood) invokeHindmoviez("JaduMovies", jaduMoviesAPI, res.id, res.season, res.episode, callback) },
+            { invokeW4U(res.en_title, res.year, res.id, res.season, res.episode, subtitleCallback, callback) },
+            // { invokeWYZIESubs(WYZIESubsAPI, res.imdbId, res.season, res.episode, subtitleCallback) },
+            { invokePrimebox(res.en_title, res.year, res.season, res.episode, subtitleCallback, callback) },
+            { invokePrimeWire(res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { if (!res.isAnime) invoke2embed(res.imdbId, res.season, res.episode, callback) },
+            { invokeSoaper(res.imdbId, res.tmdbId, res.en_title, res.season, res.episode, subtitleCallback, callback) },
+            { invokePhoenix(res.en_title, res.id, res.tmdbId, res.year, res.season, res.episode, callback) },
+            { invokeTom(res.tmdbId, res.season, res.episode, callback, subtitleCallback) },
+            { invokePrimenet(res.tmdbId, res.season, res.episode, callback) },
+            { invokePlayer4U(res.en_title, res.season, res.episode, res.airedYear, callback) },
+            { invokeThepiratebay(res.imdbId, res.season, res.episode, callback) },
+            // { if (!res.isAnime) invokeVidJoy(res.tmdbId, res.season, res.episode, callback) },
+            { invokeProtonmovies(res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { invokeAllmovieland(res.imdbId, res.season, res.episode, callback) },
+            { if(res.season == null) invokeMostraguarda(res.imdbId, subtitleCallback, callback) },
+            //{ if (!res.isBollywood || !res.isAnime) invokeMoviesflix("Moviesflix", moviesflixAPI, res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            //{ if (res.isBollywood) invokeMoviesflix("Hdmoviesflix", hdmoviesflixAPI, res.imdbId, res.season, res.episode, subtitleCallback, callback) },
+            { if (!res.isBollywood) invokeUhdmovies(res.en_title, res.year, res.season, res.episode, callback, subtitleCallback) },
+            { if (!res.isBollywood) invoke4khdhub(res.en_title, res.year, res.season, res.episode, subtitleCallback, callback) }
+        )
         return true
     }
 
@@ -255,7 +363,7 @@ class CineSimklProvider: MainAPI() {
     )
     data class LoadLinksData(
         val title: String? = null,
-        val eng_title : String? = null,
+        val en_title : String? = null,
         val tvtype: String? = null,
         val simklId: Int? = null,
         val imdbId: String? = null,
@@ -265,5 +373,9 @@ class CineSimklProvider: MainAPI() {
         val malId: Int? = null,
         val season: Int? = null,
         val episode: Int? = null,
+        val airedYear: Int? = null,
+        val isAnime: Boolean = false,
+        val isBollywood: Boolean = false,
+        val isAsian: Boolean = false
     )
 }
