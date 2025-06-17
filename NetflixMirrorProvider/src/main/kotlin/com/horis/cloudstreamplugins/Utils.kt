98365c27008b29c13d322348e62a5f77b077f4c4
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-06-17 17:35:34 +0530

    Update Utils.kt

--- NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/Utils.kt
+++ NetflixMirrorProvider/src/main/kotlin/com/horis/cloudstreamplugins/Utils.kt
@@ -14,6 +14,7 @@ import okhttp3.FormBody
 import com.lagradost.nicehttp.NiceResponse
 import kotlinx.coroutines.delay
 import android.content.Context
+import com.lagradost.api.Log
 
 val JSONParser = object : ResponseParser {
     val mapper: ObjectMapper = jacksonObjectMapper().configure(
