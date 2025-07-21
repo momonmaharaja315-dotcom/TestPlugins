075fa9042513d41d78496adaa5d0fb1ebc1ec4b5
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-07-21 09:49:11 +0530

    try

---
+++ NetflixMirrorProvider/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 24
+
+cloudstream {
+    //language = "en"
+    // All of these properties are optional, you can safely remove them
+
+    description = "Netflix, PrimeVideo, Disney Hotstar Content in Multiple Languages"
+    authors = listOf("megix")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries"
+    )
+
+    iconUrl = "https://github.com/SaurabhKaperwan/CSX/raw/refs/heads/master/NetflixMirrorProvider/icon.png"
+}
